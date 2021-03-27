47126b76b06bd05112aba6bd61dca11e5e41c062
Sahil Mehta <sahilmehta743@gmail.com>
2021-03-27 12:30:29 +0530

    Intial Commit

---
+++ movie-catalog-service/movie-catalog-service/src/main/java/com/learnmicroservices/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,8 @@
+package com.learnmicroservices.moviecatalogservice.resources;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieCatalogResource {
+
+}
